47459cb38db4b9eb333c5971bab8fb8b5226bda2
Ömer F. Yıldız <omrfrkyildiz@gmail.com>
2021-03-21 12:50:13 +0300

    JS file,functions, and events  have been added.

---
+++ app.js
@@ -0,0 +1,21 @@
+document.querySelector('form').addEventListener('submit', handleSubmitForm);
+
+function handleSubmitForm(e) {
+    e.preventDefault();
+    let input = document.querySelector('input');
+    if (input.value != '')
+        addTodo(input.value);
+    input.value = '';
+}
+
+function addTodo(todo) {
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    li.innerHTML = `
+        <span class="todo-item">${todo}</span>
+        <button name="checkButton"><i class="fas fa-check-square"></i></button>
+        <button name="deleteButton" ><i class="fas fa-trash"></i></button>
+    `;
+    li.classList.add('todo-list-item');
+    ul.appendChild(li);
+}
